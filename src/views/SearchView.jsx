673a136fd611c17e08a95a2ea7e84e188dab7186
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2021-08-25 19:51:58 -0500

    vista de busqueda terminada

--- src/views/SearchView.jsx
+++ src/views/SearchView.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { Layout, Row, Col} from 'antd';
+import { Layout, Row, Col, Input} from 'antd';
+import imgGithub from '../images/logotipo-de-github.svg';
 
 import FooterView from '../components/FooterView';
 
 const { Content } = Layout;
 
+const { Search } = Input;
+
 const SearchView = ()=>{
   return(
-    <Layout>
+    <Layout className="section-view">
       <Content>
         <Row>
-          <Col>
-           <div>
-           SABINA
-
+          <Col span={24}>
+           <div className="content-view">
+            <img className="img-logo" src={imgGithub}  alt="logo" />
+            <Search placeholder="Search" style={{ width: 200 }} />
            </div>
           </Col>
         </Row>
