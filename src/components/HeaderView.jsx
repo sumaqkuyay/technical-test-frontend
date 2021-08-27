51d098529516aab858400a4e530da0cd7481815d
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2021-08-27 03:19:27 -0500

    mejora de estilos de header

--- src/components/HeaderView.jsx
+++ src/components/HeaderView.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Layout,Input,Avatar } from 'antd';
+import { Layout,Avatar } from 'antd';
 import { GithubOutlined } from '@ant-design/icons';
 import {Link} from 'react-router-dom';
 
 
 const { Header } = Layout;
-const { Search } = Input;
 
 const FooterView = ()=>{
   return(
@@ -15,9 +14,6 @@ const FooterView = ()=>{
           <Avatar className="navbar-logo"  size={55} icon={<GithubOutlined />} />
         </Link>
         <label>GitHub</label>
-      </div>
-      <div className="navbar-right" >
-       {/* <Search placeholder="Search" style={{ width: 300 }} /> */}
       </div>
     </Header>
   );
